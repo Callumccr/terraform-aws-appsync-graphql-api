85b10aa4b63f1d1fca33e12db16419e7ca4ef068
Nicolas Martinez <nmartinez@corpit-consulting.com>
2019-05-06 21:47:08 -0300

    New output appsync_graphql_api id

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,5 @@
 output "id" {
-  value = "${aws_appsync_graphql_api.example_1.id}"
+  value = "${element(coalescelist(aws_appsync_graphql_api.example_1.*.id,aws_appsync_graphql_api.example_2.*.id,aws_appsync_graphql_api.example_3.*.id,aws_appsync_graphql_api.example_4.*.id),0)}"
+
 }
 
