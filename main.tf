183f0dbbfe2fa68ebc24893229cf91cf0565c35b
lobos1995 <entherius@gmail.com>
2019-10-29 17:12:18 -0300

    soporte a terraform 12

--- main.tf
+++ main.tf
@@ -1,54 +1,28 @@
-resource "aws_appsync_graphql_api" "example_1" {
-  count               = "${var.authentication_type == "API_KEY" ? 1 : 0}"
-  authentication_type = "${var.authentication_type}"
-  name                = "${var.name}"
-  schema              = "${var.schema}"        
-  log_config       {
-    cloudwatch_logs_role_arn  = "${var.cloudwatch_logs_role_arn}"
-    field_log_level          = "${var.field_log_level}"
+resource "aws_appsync_graphql_api" "graphql_api" {
+  authentication_type = var.authentication_type
+  name                = var.name
+  schema              = var.schema
+  
+
+  dynamic "openid_connect_config" {
+    for_each = var.openid_connect_config
+      content {
+      issuer = lookup(openid_connect_config.value,"issuer",null)
+    }
   }
-}
-
-
-resource "aws_appsync_graphql_api" "example_2" {
-  count               = "${var.authentication_type == "AMAZON_COGNITO_USER_POOLS" ? 1 : 0}"
-  authentication_type = "${var.authentication_type}"
-  name                = "${var.name}"
-  schema              = "${var.schema}"
-  user_pool_config {
-    aws_region     = "${var.aws_region}"
-    default_action = "${var.default_action}"
-    user_pool_id   = "${var.user_pool_id}"
+  dynamic "user_pool_config" {
+    for_each = var.user_pool_config
+      content {
+        aws_region     = lookup(user_pool_config.value,"aws_region",null)
+        default_action = lookup(user_pool_config.value,"default_action",null)
+        user_pool_id   = lookup(user_pool_config.value,"user_pool_id",null)
+      }
   }
-  log_config       {
-    cloudwatch_logs_role_arn  = "${var.cloudwatch_logs_role_arn}"
-    field_log_level          = "${var.field_log_level}"
-  }
-}
-
-
-resource "aws_appsync_graphql_api" "example_3" {
-  count               = "${var.authentication_type == "AWS_IAM" ? 1 : 0}"
-  authentication_type = "${var.authentication_type}"
-  name                = "${var.name}"
-  log_config       {
-    cloudwatch_logs_role_arn  = "${var.cloudwatch_logs_role_arn}"
-    field_log_level          = "${var.field_log_level}"
-  }
-  schema = "${var.schema}"        
-}
-
-
-resource "aws_appsync_graphql_api" "example_4" {
-  count               = "${var.authentication_type == "OPENID_CONNECT" ? 1 : 0}"
-  authentication_type = "${var.authentication_type}"
-  name                = "${var.name}"
-  schema              = "${var.schema}"        
-  log_config       {
-    cloudwatch_logs_role_arn  = "${var.cloudwatch_logs_role_arn}"
-    field_log_level          = "${var.field_log_level}"
-  }
-  openid_connect_config {
-    issuer = "https://example.com"
+  dynamic "log_config" {
+    for_each = var.log_config
+      content {
+        cloudwatch_logs_role_arn = lookup(log_config.value,"cloudwatch_logs_role_arn",null)
+        field_log_level          = lookup(log_config.value,"field_log_level",null)
+      }
   }
 }
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,4 @@
 output "id" {
-  value = "${element(coalescelist(aws_appsync_graphql_api.example_1.*.id,aws_appsync_graphql_api.example_2.*.id,aws_appsync_graphql_api.example_3.*.id,aws_appsync_graphql_api.example_4.*.id),0)}"
-
+  value = aws_appsync_graphql_api.graphql_api.id
 }
 
