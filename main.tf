bd42e5817b7acd2375ea51b47f08990e5e069ff6
Callum <callumccr@gmail.com>
2020-08-26 18:05:55 +0100

    Gitlab README commit

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ resource "aws_appsync_graphql_api" "api_key" {
   dynamic "additional_authentication_provider" {
     for_each = length(list(var.additional_authentication_provider)) > 0 ? var.additional_authentication_provider : {}
     content {
-      authentication_type = each.value.authentication_type
+      authentication_type = each.value["authentication_type"]
       user_pool_config {
-        user_pool_id        = each.value.user_pool_config.user_pool_id
-        app_id_client_regex = each.value.user_pool_config.app_id_client_regex
+        user_pool_id        = each.value.user_pool_config["user_pool_id"]
+        app_id_client_regex = each.value.user_pool_config["app_id_client_regex"]
         aws_region          = var.aws_region
       }
     }
